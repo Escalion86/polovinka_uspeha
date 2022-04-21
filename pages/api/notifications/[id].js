2c2c50662eacc6ffe8dca036795deeea9d800de8
Escalion <escalion86@gmail.com>
2022-04-21 23:57:39 +0700

    fix

--- pages/api/notifications/[id].js
+++
@@ -1,6 +0,0 @@
-import Notifications from '@models/Notifications'
-import CRUD from '@server/CRUD'
-
-export default async function handler(req, res) {
-  return await CRUD(Notifications, req, res)
-}
