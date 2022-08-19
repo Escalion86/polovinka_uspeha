e4289ff62046c00fa4ff5c323ec13aa3cace216c
Escalion <escalion86@gmail.com>
2022-08-19 15:08:02 +0700

    Fixed fetchingLog

--- helpers/fetchers.js
+++ helpers/fetchers.js
@@ -97,7 +97,7 @@ export async function fetchingUserByPhone(
 export async function fetchingLog(data, domen = process.env.NEXTAUTH_SITE) {
   console.log('Запущен fetchingLog')
   const resp = await fetch(`${domen}/api/events`, {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
     // mode: 'cors', // no-cors, *cors, same-origin
     // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     // credentials: 'same-origin', // include, *same-origin, omit
