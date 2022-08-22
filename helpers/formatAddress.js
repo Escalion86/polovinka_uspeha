0bbaebad4d51163c9c791f1e9326e2bc09acd42b
Escalion <escalion86@gmail.com>
2022-08-22 12:24:33 +0700

    Rebuild address formating

--- helpers/formatAddress.js
+++ helpers/formatAddress.js
@@ -1,21 +1,19 @@
 const formatAddress = (address, textIfNoAddress) => {
   if (!address?.town && !address?.street) return textIfNoAddress ?? ''
-  return `${
-    address?.town &&
-    address.town + (address?.street ? ', ' + address.street : '')
-  }${
-    (address?.house ? ' ' + address.house : '') +
-    (address.house && address.flat ? ' - ' + address.flat : '')
-  }${
-    address?.floor || address?.entrance
-      ? ' (' +
-        (address?.entrance ? `${address?.entrance} подъезд` : '') +
-        (address?.floor
-          ? `${address?.entrance ? ', ' : ''}${address?.floor} этаж`
-          : '') +
-        ')'
-      : ''
-  }`
+
+  return (
+    [
+      address?.town,
+      address?.street,
+      // [address?.house, address?.flat].filter((data) => data).join(' - '),
+      address?.house ? `дом ${address?.house}` : '',
+      address?.flat ? `кв. ${address?.flat}` : '',
+      address?.entrance ? `${address?.entrance} подъезд` : '',
+      address?.floor ? `${address?.floor} этаж` : '',
+    ]
+      .filter((data) => data)
+      .join(', ') + (address?.comment ? ' (' + address.comment + ')' : '')
+  )
 }
 
 export default formatAddress
