9666cc2a3e6a7c3ac6288f191136f4ec59af4a49
Escalion <escalion86@gmail.com>
2022-10-05 01:55:20 +0700

    Update TabList

--- components/Tabs/TabList.js
+++ components/Tabs/TabList.js
@@ -1,6 +1,18 @@
 import { TabList as MuiTabList } from '@mui/lab'
 import cn from 'classnames'
 
+import { TabScrollButton } from '@mui/material'
+import { styled } from '@mui/styles'
+
+const MyTabScrollButton = styled(TabScrollButton)({
+  '&.Mui-disabled': {
+    width: 0,
+  },
+  overflow: 'hidden',
+  transition: 'width 0.5s',
+  width: 34,
+})
+
 const TabList = ({
   variant = 'fullWidth',
   className,
@@ -10,6 +22,7 @@ const TabList = ({
   allowScrollButtonsMobile,
 }) => (
   <MuiTabList
+    ScrollButtonComponent={MyTabScrollButton}
     variant={variant}
     onChange={(e, newValue) => onChange(newValue)}
     className={cn('border-b border-gray-600', className)}
