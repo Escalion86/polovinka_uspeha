33ec8123f5c1dda7b09802a95b34cb76f95eaac8
Escalion <escalion86@gmail.com>
2022-10-26 00:02:37 +0700

    Updated ValueItem

--- components/ValuePicker/ValueItem.js
+++ components/ValuePicker/ValueItem.js
@@ -9,12 +9,14 @@ const ValueItem = ({
   icon = null,
   onClick = null,
   hoverable,
+  className,
 }) => (
   <button
     className={cn(
       `h-[30px] flex min-w-22 duration-300 outline-none items-center justify-center border px-2 py-0.5 rounded cursor-pointer gap-x-2 flex-nowrap border-${color} group`,
       active ? `text-white bg-${color}` : `text-${color} bg-white`,
-      hoverable ? `hover:text-white hover:bg-${color}` : ''
+      hoverable ? `hover:text-white hover:bg-${color}` : '',
+      className
     )}
     onClick={() => onClick && onClick(value)}
   >
