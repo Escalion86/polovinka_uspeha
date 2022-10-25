3464f8f50c599304918beed014999b7dc905d2f2
Escalion <escalion86@gmail.com>
2022-10-25 16:24:49 +0700

    test

---
+++ helpers/getUserFullName.js
@@ -0,0 +1,10 @@
+const getUserFullName = (user) => {
+  const array = []
+  if (user?.firstName) array.push(upperCaseFirst(user.firstName))
+  if (user?.thirdName) array.push(upperCaseFirst(user.thirdName))
+  if (user?.secondName) array.push(upperCaseFirst(user.secondName))
+
+  return array.join(' ')
+}
+
+export default getUserFullName
