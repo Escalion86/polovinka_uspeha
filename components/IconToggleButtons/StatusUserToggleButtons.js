ac48bd4d9ba95ba1afa413a042694174c8c33d92
Escalion <escalion86@gmail.com>
2023-01-08 00:34:06 +0700

    Updated StatusUserToggle

--- components/IconToggleButtons/StatusUserToggleButtons.js
+++ components/IconToggleButtons/StatusUserToggleButtons.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, ButtonGroup } from '@mui/material'
 import {
+  faBan,
   faGenderless,
   faMars,
   faUser,
@@ -15,8 +16,10 @@ const StatusUserToggleButtons = ({ value, onChange }) => {
       <Button
         onClick={() =>
           onChange({
-            novice: value.novice || value.member,
+            novice:
+              !value.novice && value.member && !value.ban ? true : value.novice,
             member: !value.member,
+            ban: value.ban,
           })
         }
         variant={value.member ? 'contained' : 'outlined'}
@@ -32,7 +35,9 @@ const StatusUserToggleButtons = ({ value, onChange }) => {
         onClick={() =>
           onChange({
             novice: !value.novice,
-            member: value.novice || value.member,
+            member:
+              value.novice && !value.member && !value.ban ? true : value.member,
+            ban: value.ban,
           })
         }
         variant={value.novice ? 'contained' : 'outlined'}
@@ -46,6 +51,27 @@ const StatusUserToggleButtons = ({ value, onChange }) => {
           // )}
         > */}
         <FontAwesomeIcon className="w-6 h-6" icon={faUser} />
+      </Button>
+      <Button
+        onClick={() =>
+          onChange({
+            novice:
+              !value.member && !value.novice && value.ban ? true : value.novice,
+            member: value.member,
+            ban: !value.ban,
+          })
+        }
+        variant={value.ban ? 'contained' : 'outlined'}
+        color="red"
+        className={value.ban ? 'text-white' : 'text-red-400'}
+      >
+        {/* <div
+          // className={cn(
+          //   'w-8 flex justify-center items-center',
+          //   userGender ? 'bg-' + userGender.color : 'bg-gray-400'
+          // )}
+        > */}
+        <FontAwesomeIcon className="w-6 h-6" icon={faBan} />
       </Button>
     </ButtonGroup>
   )
