fb59adc2a81dd5fd2d252b1a80446a6e6001e2f7
Escalion <escalion86@gmail.com>
2023-04-30 21:07:49 +0700

    Added yandex metric

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,32 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+           
+              ym(93417188, "init", {
+                   clickmap:true,
+                   trackLinks:true,
+                   accurateTrackBounce:true,
+                   webvisor:true
+              });`,
+            }}
+          ></script>
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/93417188"
+                style={{ position: 'absolute', left: -9999 }}
+                alt=""
+              />
+            </div>
+          </noscript>
         </body>
       </Html>
     )
