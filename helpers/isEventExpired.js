a29048d514c806b82f674a7b2a67c0fe4225f04f
Escalion <escalion86@gmail.com>
2023-07-17 02:20:47 +0700

    Added server dateTime for real view date

--- helpers/isEventExpired.js
+++ helpers/isEventExpired.js
@@ -5,7 +5,7 @@ import serverSettingsAtom from '@state/atoms/serverSettingsAtom'
 
 const isEventExpired = (event) => {
   const serverDate = new Date(getRecoil(serverSettingsAtom)?.dateTime)
-  getDiffBetweenDates(event?.dateEnd, serverDate) >= 0
+  return getDiffBetweenDates(event?.dateEnd, serverDate) >= 0
 }
 // getMinutesBetween(event.date) >= (event.duration ?? 0)
 
