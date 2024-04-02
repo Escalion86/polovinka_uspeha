0a288e32c9961ec6fa8c069252268d817173ca07
Escalion <escalion86@gmail.com>
2024-04-02 23:08:22 +0700

    PriceDiscount fix

--- components/PriceDiscount.js
+++ components/PriceDiscount.js
@@ -39,80 +39,80 @@ const PriceDiscount = ({
 
   return (
     <div className={cn('flex flex-wrap items-center gap-x-1', className)}>
-      {item.price ? (
-        eventPriceForUser === item.price / 100 ? (
-          <div className="flex items-center text-lg font-bold laptop:text-xl gap-x-1 flex-nowrap">
-            {prefix && <span className="font-bold tablet:block">{prefix}</span>}
-            <span className="whitespace-nowrap">{item.price / 100 + ' ₽'}</span>
-          </div>
-        ) : (
-          <div
+      {/* {item.price ? ( */}
+      {/* // eventPriceForUser === item.price / 100 ? (
+        //   <div className="flex items-center text-lg font-bold laptop:text-xl gap-x-1 flex-nowrap">
+        //     {prefix && <span className="font-bold tablet:block">{prefix}</span>}
+        //     <span className="whitespace-nowrap">{item.price / 100 + ' ₽'}</span>
+        //   </div>
+        // ) : ( */}
+      <div
+        className={cn('flex gap-x-1', vertical ? 'items-center' : 'items-end')}
+      >
+        {prefix && (
+          <span
             className={cn(
-              'flex gap-x-1',
-              vertical ? 'items-center' : 'items-end'
+              'text-base font-bold flex items-center',
+              vertical ? 'mt-3.5' : 'h-7'
             )}
           >
-            {prefix && (
-              <span
-                className={cn(
-                  'text-base font-bold flex items-center',
-                  vertical ? 'mt-3.5' : 'h-7'
-                )}
-              >
-                {prefix}
-              </span>
-            )}
-            <div
-              className={cn(
-                vertical ? '' : 'flex items-center gap-x-1 laptop:gap-x-2',
-                mobileVertical || vertical ? 'relative' : ''
-              )}
-            >
-              {item.usersStatusDiscountResult &&
-              item.usersStatusDiscountResult[
-                fixedUserStatus === 'member' ? 'memberFrom' : 'noviceFrom'
-              ]
-                ? ' от'
-                : !priceForStatus && (
-                    <div
-                      className={cn(
-                        'text-sm laptop:text-base text-center whitespace-normal',
-                        {
-                          'absolute top-0 left-0 right-0': vertical,
-                        },
-                        mobileVertical
-                          ? 'absolute laptop:block top-0 left-0 right-0 laptop:top-auto laptop:left-auto laptop:right-auto laptop:relative'
-                          : ''
-                      )}
-                    >
-                      <div className="relative">
-                        <div className="whitespace-nowrap">
-                          {item.price / 100 + ' ₽'}
-                        </div>
-                        <div className="absolute top-[9px] laptop:top-[11px] left-0 right-0 transform rotate-15 border-b-1 laptop:border-b-2 border-danger" />
-                        <div className="absolute top-[9px] laptop:top-[11px] left-0 right-0 transform -rotate-15 border-b-1 laptop:border-b-2 border-danger" />
-                      </div>
-                    </div>
+            {prefix}
+          </span>
+        )}
+        <div
+          className={cn(
+            vertical ? '' : 'flex items-center gap-x-1 laptop:gap-x-2',
+            mobileVertical || vertical ? 'relative' : ''
+          )}
+        >
+          {item.usersStatusDiscountResult &&
+          item.usersStatusDiscountResult[
+            fixedUserStatus === 'member' ? 'memberFrom' : 'noviceFrom'
+          ]
+            ? ' от'
+            : !priceForStatus &&
+              !!item.price && (
+                <div
+                  className={cn(
+                    'text-sm laptop:text-base text-center whitespace-normal',
+                    {
+                      'absolute top-0 left-0 right-0': vertical,
+                    },
+                    mobileVertical
+                      ? 'absolute laptop:block top-0 left-0 right-0 laptop:top-auto laptop:left-auto laptop:right-auto laptop:relative'
+                      : ''
                   )}
-              <div
-                className={cn(
-                  'whitespace-nowrap text-lg laptop:text-xl font-bold',
-                  {
-                    'mt-3.5': vertical,
-                  },
-                  mobileVertical ? 'mt-3 laptop:mt-0' : ''
-                )}
-              >
-                {eventPriceForUser + ' ₽'}
-              </div>
-            </div>
+                >
+                  <div className="relative">
+                    <div className="whitespace-nowrap">
+                      {item.price / 100 + ' ₽'}
+                    </div>
+                    <div className="absolute top-[9px] laptop:top-[11px] left-0 right-0 transform rotate-15 border-b-1 laptop:border-b-2 border-danger" />
+                    <div className="absolute top-[9px] laptop:top-[11px] left-0 right-0 transform -rotate-15 border-b-1 laptop:border-b-2 border-danger" />
+                  </div>
+                </div>
+              )}
+          <div
+            className={cn(
+              'whitespace-nowrap text-lg laptop:text-xl font-bold',
+              {
+                'mt-3.5': vertical,
+              },
+              mobileVertical ? 'mt-3 laptop:mt-0' : '',
+              eventPriceForUser ? 'uppercase' : ''
+            )}
+          >
+            {eventPriceForUser ? eventPriceForUser + ' ₽' : 'Бесплатно'}
           </div>
-        )
-      ) : (
-        <div className="text-lg font-bold uppercase whitespace-normal">
-          Бесплатно
         </div>
-      )}
+      </div>
+      {/* // )
+      //  : (
+      //   // )
+      //   <div className="text-lg font-bold uppercase whitespace-normal">
+      //     Бесплатно
+      //   </div>
+      // )} */}
     </div>
   )
 }
