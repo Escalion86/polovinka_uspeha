b388498cccaf316240f2ba8700ec6c72ab68a0f8
Escalion <escalion86@gmail.com>
2024-07-03 01:48:32 +0700

    Update getDaysBetween

--- helpers/getDaysBetween.js
+++ helpers/getDaysBetween.js
@@ -4,7 +4,9 @@ const getDaysBetween = (
   date1 = new Date(),
   date2 = new Date(),
   showNoun = true,
-  removeTime = false
+  removeTime = false,
+  ceil = false, // Округлить в большую сторону
+  resultAdd = 0
 ) => {
   if (!date1) return null
   var difference = removeTime
@@ -12,8 +14,10 @@ const getDaysBetween = (
       new Date(new Date().toDateString()).getTime()
     : new Date(date1).getTime() - new Date(date2).getTime()
 
-  const result = Math.floor(difference / (1000 * 3600 * 24))
-  if (showNoun) return getNoun(result, 'день', 'дня', 'дней', true)
+  const result = ceil
+    ? Math.ceil(difference / (1000 * 3600 * 24))
+    : Math.floor(difference / (1000 * 3600 * 24))
+  if (showNoun) return getNoun(result + resultAdd, 'день', 'дня', 'дней', true)
   else return result
 }
 
