69e45d66b5643566478a205ae5c4c7ced25967ab
Escalion <escalion86@gmail.com>
2025-01-10 16:47:19 +0700

    Fixes in async atoms

--- state/async/asyncEventsUsersAllAtom.js
+++ state/async/asyncEventsUsersAllAtom.js
@@ -1,10 +1,10 @@
-import { atom } from 'jotai'
+import { atomWithDefault } from 'jotai/utils'
 
 import { getData } from '@helpers/CRUD'
 import isLoadedAtom from '@state/atoms/isLoadedAtom'
 import store from '../store'
 
-const asyncEventsUsersAllAtom = atom(async (get) => {
+const asyncEventsUsersAllAtom = atomWithDefault(async (get) => {
   const res = await getData('/api/eventsusers', null, null, null, false)
   store.set(isLoadedAtom('asyncEventsUsersAllAtom'), true)
 
