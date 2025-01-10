1f89f23eee428ba4906a17d858ceb9adb4307382
Escalion <escalion86@gmail.com>
2025-01-10 16:21:27 +0700

    Fixes in async atoms

--- state/async/asyncPaymentsAtom.js
+++ state/async/asyncPaymentsAtom.js
@@ -1,10 +1,10 @@
-import { atom } from 'jotai'
+import { atomWithDefault } from 'jotai/utils'
 
 import store from '../store'
 import isLoadedAtom from '@state/atoms/isLoadedAtom'
 import { getData } from '@helpers/CRUD'
 
-const asyncPaymentsAtom = atom(async () => {
+const asyncPaymentsAtom = atomWithDefault(async () => {
   const res = await getData('/api/payments', null, null, null, false)
   store.set(isLoadedAtom('asyncPaymentsAtom'), true)
 
--- state/async/asyncServicesUsersAtom.js
+++ state/async/asyncServicesUsersAtom.js
@@ -1,10 +1,10 @@
-import { atom } from 'jotai'
+import { atomWithDefault } from 'jotai/utils'
 
 import store from '../store'
 import isLoadedAtom from '@state/atoms/isLoadedAtom'
 import { getData } from '@helpers/CRUD'
 
-const asyncServicesUsersAtom = atom(async () => {
+const asyncServicesUsersAtom = atomWithDefault(async () => {
   const res = await getData('/api/servicesusers', null, null, null, false)
   store.set(isLoadedAtom('asyncServicesUsersAtom'), true)
 
