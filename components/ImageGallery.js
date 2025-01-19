4ad3ffd5d58579502845fc553153c58774ff5982
Escalion <escalion86@gmail.com>
2025-01-20 03:58:24 +0700

    Rebuild ImageGallery

--- components/ImageGallery.js
+++ components/ImageGallery.js
@@ -13,25 +13,25 @@ const ImageWithLoading = ({ original, originalClass }) => {
   // const [isError, setIsError] = useState(false)
 
   return (
-    <div className="flex items-center justify-center object-contain h-full max-h-screen min-w-full min-h-full">
+    <div className="relative flex items-center justify-center object-contain h-full max-h-screen min-w-full min-h-full">
       {/* {!isError ? ( */}
-      <div className="relative min-h-full h-full aspect-[16/9]">
-        <LoadingSpinner
-          className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-          size="lg"
-        />
-        <Image
-          alt="image"
-          src={original}
-          width="0"
-          height="0"
-          sizes="100vw"
-          className={cn(originalClass, 'object-contain relative')}
-          // onLoad={() => setIsLoaded(true)}
-          // onError={() => setIsError(true)}
-          priority
-        />
-      </div>
+      {/* <div className="relative min-h-full h-full aspect-[16/9]"> */}
+      <LoadingSpinner
+        className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+        size="lg"
+      />
+      <Image
+        alt="image"
+        src={original}
+        width="0"
+        height="0"
+        sizes="100vw"
+        className={cn(originalClass, 'object-contain relative')}
+        // onLoad={() => setIsLoaded(true)}
+        // onError={() => setIsError(true)}
+        priority
+      />
+      {/* </div> */}
       {/* ) : (
         <div className="h-full aspect-[16/9] flex justify-center items-center">
           Ошибка загрузки фотографии
@@ -82,6 +82,7 @@ const ImageGallery = ({ images, noImage, className }) => {
           showPlayButton={false}
           showFullscreenButton={true}
           useBrowserFullscreen={false}
+          additionalClass="aspect-[16/9]"
           // additionalClass={cn(
           //   'w-full max-h-60 laptop:max-h-80 max-w-full',
           //   className
