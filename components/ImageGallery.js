fb79bf326182527cb0978aa3659df3826b582516
Escalion <escalion86@gmail.com>
2025-01-22 15:22:28 +0700

    Fix ImageGallery size

--- components/ImageGallery.js
+++ components/ImageGallery.js
@@ -57,7 +57,7 @@ const ImageGallery = ({ images, noImage, className }) => {
 
   return (
     <div className="flex justify-center w-full">
-      <div className="flex justify-center border border-gray-400 h-60 laptop:h-80 max-h-60 laptop:max-h-80 aspect-[16/9] rounded-2xl overflow-hidden shadow-sm">
+      <div className="flex justify-center border border-gray-400 phoneH:h-60 laptop:h-80 phoneH:max-h-60 laptop:max-h-80 aspect-[16/9] rounded-2xl overflow-hidden shadow-sm">
         {/* // {images?.length > 0 ? (
           // <ControlledZoom
           //   zoomMargin={20}
