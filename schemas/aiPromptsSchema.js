78ec33557fb267d5b606767af3bf90a446c5fa31
Aleksei Belinskiy <escalion86@gmail.com>
2025-11-18 17:38:33 +0700

    Add saving and loading AI prompts

---
+++ schemas/aiPromptsSchema.js
@@ -0,0 +1,20 @@
+const aiPromptsSchema = {
+  title: {
+    type: String,
+    default: null,
+  },
+  prompt: {
+    type: String,
+    default: null,
+  },
+  section: {
+    type: String,
+    default: null,
+  },
+  userId: {
+    type: String,
+    default: null,
+  },
+}
+
+export default aiPromptsSchema
